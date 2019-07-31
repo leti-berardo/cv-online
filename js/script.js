0a43b975d681636151852f90bc800056c63584c7
Leticia Berardo <leticiaberardo@gmail.com>
2019-07-31 16:01:08 -0300

    mejora de las funciones en jquery final. Muestra y oculta las secciones

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 document.getElementById("about-me").addEventListener("click", moveAboutMe);
-// document.getElementById("formacion-institucional").addEventListener("click", moveFormInst);
-// document.getElementById("experiencia-laboral").addEventListener("click", moveExpLab);
-// document.getElementById("skills").addEventListener("click", moveSkills);
-// document.getElementById("cursos-autodidactas").addEventListener("click", moveCursosAuto);
+document.getElementById("formacion-institucional").addEventListener("click", moveFormInst);
+document.getElementById("experiencia-laboral").addEventListener("click", moveExpLab);
+document.getElementById("skills").addEventListener("click", moveSkills);
+document.getElementById("cursos-autodidactas").addEventListener("click", moveCursosAuto);
 
 function moveAboutMe() {
     $('#main-content section').hide();
     $('#about-me-section').show();
 };
+
+function moveFormInst() {
+    $('#main-content section').hide();
+    $('#formacion-institucional-section').show();
+};
+
+function moveExpLab() {
+    $('#main-content section').hide();
+    $('#experiencia').show();
+};
+
+function moveSkills() {
+    $('#main-content section').hide();
+    $('section#skills').show();
+};
+
+function moveCursosAuto() {
+    $('#main-content section').hide();
+    $('section#cursos-autodidactas').show();
+};
+
